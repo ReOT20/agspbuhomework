97dfaa2ae70ad13b9b152c59e36b1339b93e9073
ReOT20 <ReOT20@users.noreply.github.com>
2019-11-04 20:00:50 +0300

    7 task

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,70 +1,27 @@
 ﻿#include <iostream>
 
-int** put_snake(int** Snake, int n, int m) {
-	int curX = m - 1;
-	int curY = 0;
-	int direction = 2;
-	int top_border = 0;
-	int left_border = 0;
-	int bottom_border = n - 1;
-	int right_border = m - 1;
-	int i = n * m - 1;
-	Snake[curY][curX] = i;
-	while (i > 0) {
-		if (direction == 2) {
-			curX--;
-			if (curX == left_border) {
-				direction = 3;
-				top_border++;
-			}
+int abs(int x) {
+	if (x < 0) return (x * -1);
+	return x;
+}
+
+int proper_fraction(int n) {
+	int res = n;
+	int i = 2;
+	while (i * i <= n) {
+		if (n % i == 0) {
+			while (n % i == 0) n /= i;
+			res -= res/i;
 		}
-		else if (direction == 3) {
-			curY++;
-			if (curY == bottom_border) {
-				direction = 4;
-				left_border++;
-			}
-		}
-		else if (direction == 4) {
-			curX++;
-			if (curX == right_border) {
-				direction = 1;
-				bottom_border--;
-			}
-		}
-		else {
-			curY--;
-			if (curY == top_border) {
-				direction = 2;
-				right_border--;
-			}
-		}
-		i--;
-		Snake[curY][curX] = i;
+		i++;
 	}
-	return Snake;
+	if (n > 1) return (res - res/n)*2;
+	return res*2;
 }
 
 int main() {
-	int n, m;
-	std::cin >> n >> m;
-	int** Snake = new int* [n];
-	int i = 0;
-	while (i < n) {
-		Snake[i] = new int[m];
-		i++;
-	}
-	Snake = put_snake(Snake, n, m);
-	int j;
-	i = 0;
-	while (i < n) {
-		j = 0;
-		while (j < m) {
-			std::cout << Snake[i][j] << "\t";
-			j++;
-		}
-		std::cout << "\n";
-		i++;
-	}
+	int n;
+	std::cin >> n;
+	std::cout << proper_fraction(abs(n));
 	return 0;
 }
---
+++ Homework_autumn_hollidays_2019/7.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int abs(int x) {
+	if (x < 0) return (x * -1);
+	return x;
+}
+
+int proper_fraction(int n) {
+	int res = n;
+	int i = 2;
+	while (i * i <= n) {
+		if (n % i == 0) {
+			while (n % i == 0) n /= i;
+			res -= res / i;
+		}
+		i++;
+	}
+	if (n > 1) return (res - res / n) * 2;
+	return res * 2;
+}
+
+int main() {
+	int n;
+	std::cin >> n;
+	std::cout << proper_fraction(abs(n));
+	return 0;
+}
