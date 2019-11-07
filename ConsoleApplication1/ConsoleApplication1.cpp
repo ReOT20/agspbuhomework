c80d01ed0fc89808b6799f36f7b7691f2e70c3e6
ReOT20 <ReOT20@users.noreply.github.com>
2019-11-07 11:59:43 +0300

    10 task beta
    Работает полностью правильно только с компилятором cling

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,80 +1,61 @@
 ﻿#include <iostream>
 #include <cstdlib>
 
-int sum(int* arr) {
-	int i = 2;
-	int s = 0;
+unsigned long int sum(unsigned long int* arr) {
+	unsigned long int i = 2;
+	unsigned long int s = 0;
 	while (i < 10) {
-		s += arr[i]* arr[i]* arr[i];
+		s += arr[i] * arr[i] * arr[i];
 		i++;
 	}
 	return s;
 }
 
-int* cut(int* Array, int a, int b) {
-	int i = a;
-	int* arr = new int[b - a];
-	while (i < b) {
-		arr[i - a] = Array[i];
-		i++;
-	}
-	return arr;
-}
-
-int crt(int n) {
-	int i = n;
+unsigned long int crt(unsigned long int n) {
+	unsigned long int i = n;
 	while (i * i * i > n) i--;
 	return i;
 }
 
-int cub(int* arr) {
-	if (arr[1] == 11) {
-		//std::cout << arr[2] << " ";
-		//std::cout << arr[3] << " ";
-		//std::cout << arr[4] << " ";
-		//std::cout << arr[5] << " ";
-		//std::cout << arr[6] << " ";
-		//std::cout << arr[7] << " ";
-		//std::cout << arr[8] << " ";
-		//std::cout << arr[9] << " ";
-		//std::cout << arr[10] << " ";
-		//std::cout << sum(arr) << " ";
-		//std::cout << arr[12] << " ";
+unsigned long int cub(unsigned long int* arr) {
+	if (arr[1] > 10) {
 		if (sum(arr) == arr[12]) {
-			int i = 2;
-			int s = 0;
-			while (s != arr[12]) {
+			unsigned long int i = 2;
+			unsigned long int s = 0;
+			while (s != arr[12] && i < 10) {
 				s += arr[i] * arr[i] * arr[i];
 				std::cout << arr[i] << " ";
 				i++;
 			}
 			if (arr[12] == 0) std::cout << 0;
-			//std::cin >> i;
 			exit(0);
 		}
-		return 0;
 	}
-	int n = crt(arr[0]);
-	int* Array = new int[12];
-	while (n >= 0) {
-		Array = arr;
-		Array[arr[1]] = n;
-		Array[0] -= n * n * n;
-		Array[1]++;
-		cub(Array);
-		n--;
+	else {
+		unsigned long int n = crt(arr[0]);
+		unsigned long int* Array = new unsigned long int[12];
+		while (n >= 0) {
+			if (arr[1] > 10) return 0;
+			Array = arr;
+			Array[arr[1]] = n;
+			Array[0] -= n * n * n;
+			Array[1]++;
+			cub(Array);
+			n--;
+		}
+
 	}
 	return 0;
 }
 
 int main() {
-	int n;
+	unsigned long int n;
 	std::cin >> n;
-	int* arr = new int[12];
+	unsigned long int* arr = new unsigned long int[12];
 	arr[0] = n;
 	arr[1] = 2;
 	arr[12] = n;
-	int i = 2;
+	unsigned long int i = 2;
 	while (i < 12) {
 		arr[i] = -1;
 		i++;
---
+++ Homework_autumn_hollidays_2019/10.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cstdlib>
+
+unsigned long int sum(unsigned long int* arr) {
+	unsigned long int i = 2;
+	unsigned long int s = 0;
+	while (i < 10) {
+		s += arr[i] * arr[i] * arr[i];
+		i++;
+	}
+	return s;
+}
+
+unsigned long int crt(unsigned long int n) {
+	unsigned long int i = n;
+	while (i * i * i > n) i--;
+	return i;
+}
+
+unsigned long int cub(unsigned long int* arr) {
+	if (arr[1] > 10) {
+		if (sum(arr) == arr[12]) {
+			unsigned long int i = 2;
+			unsigned long int s = 0;
+			while (s != arr[12] && i < 10) {
+				s += arr[i] * arr[i] * arr[i];
+				std::cout << arr[i] << " ";
+				i++;
+			}
+			if (arr[12] == 0) std::cout << 0;
+			exit(0);
+		}
+	}
+	else {
+		unsigned long int n = crt(arr[0]);
+		unsigned long int* Array = new unsigned long int[12];
+		while (n >= 0) {
+			if (arr[1] > 10) return 0;
+			Array = arr;
+			Array[arr[1]] = n;
+			Array[0] -= n * n * n;
+			Array[1]++;
+			cub(Array);
+			n--;
+		}
+
+	}
+	return 0;
+}
+
+int main() {
+	unsigned long int n;
+	std::cin >> n;
+	unsigned long int* arr = new unsigned long int[12];
+	arr[0] = n;
+	arr[1] = 2;
+	arr[12] = n;
+	unsigned long int i = 2;
+	while (i < 12) {
+		arr[i] = -1;
+		i++;
+	}
+	cub(arr);
+	std::cout << "Impossible";
+	return 0;
+}
