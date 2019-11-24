2a0d2e29f5dda2db8c5dfa6e4a077719e8612026
ReOT20 <ReOT20@users.noreply.github.com>
2019-11-25 00:13:59 +0300

    1 and 3 tasks

---
+++ 23.11.2019.HW/2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+	long n;
+	std::cin >> n;
+	long i = 0;
+	int* arr = new int[n * 2];
+	while (i < n * 2) {
+		arr[i] = 1;
+		i++;
+	}
+	i = 1;
+	while (i <= long(sqrt(n * 2)) + 1) {
+		long j = i + i + 2;
+		while (j < n * 2 + 2) {
+			arr[j - 1] = 0;
+			j += i + 1;
+		}
+		i += 1;
+	}
+	i = 3;
+	long j = 0;
+	while (i < n * 2 && j < n) {
+		if (arr[i] == 0) {
+			std::cout << i + 1 << " ";
+			j++;
+		}
+		i++;
+	}
+	return 0;
+}
+
+/*Напишите программу находящую первые n составных чисел.*/
--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,61 +1,34 @@
 ﻿#include <iostream>
-
-int** build_ziggurat(int n, int** Ziggurat) {
-	int i = 0;
-	int j;
-	int count = 0;
-	while (i < n - n / 2) {
-		j = 0;
-		int k = 1;
-		while (j < n) {
-			Ziggurat[count][j] = k;
-			if (j < (n - n / 2) && k < i + 1) k++;
-			else if ((j >= (n - n / 2) && k >= n - j) || (k == n - n / 2 && n % 2 == 1)) k--;
-			j++;
-		}
-		count++;
-		i++;
-	}
-	i = n / 2 - 1;
-	while (i >= 0) {
-		j = 0;
-		int k = 1;
-		while (j < n) {
-			Ziggurat[count][j] = k;
-			if (j < (n - n / 2) && k < i + 1) {
-				k++;
-			}
-			else if ((j >= (n - n / 2) && k >= n - j) || (k == n - n / 2 && n % 2 == 1)) {
-				k--;
-			}
-			j++;
-		}
-		count++;
-		i--;
-	}
-	return Ziggurat;
-}
+#include <cmath>
 
 int main() {
-	int n;
+	long n;
 	std::cin >> n;
-	int** Ziggurat = new int* [n];
-	int i = 0;
-	while (i < n) {
-		Ziggurat[i] = new int[n];
+	long i = 0;
+	int* arr = new int[n*2];
+	while (i < n*2) {
+		arr[i] = 1;
 		i++;
 	}
-	Ziggurat = build_ziggurat(n, Ziggurat);
-	i = 0;
-	int j;
-	while (i < n) {
-		j = 0;
-		while (j < n) {
-			std::cout << Ziggurat[i][j] << " ";
+	i = 1;
+	while (i <= long(sqrt(n*2))+1) {
+		long j = i + i + 2;
+		while (j < n*2 + 2) {
+			arr[j - 1] = 0;
+			j += i + 1;
+		}
+		i += 1;
+	}
+	i = 3;
+	long j = 0;
+	while (i < n*2 && j < n) {
+		if (arr[i] == 0) {
+			std::cout << i + 1 << " ";
 			j++;
 		}
-		std::cout << "\n";
 		i++;
 	}
 	return 0;
 }
+
+/*Напишите программу находящую первые n составных чисел.*/
