599714e593c4e46419dad33c1f5eb5ef1985d741
ReOT20 <ReOT20@users.noreply.github.com>
2019-11-29 23:19:25 +0300

    1, 2, 3, 5, 8 tasks fixes

---
+++ 23.11.2019.HW/5.cpp
@@ -0,0 +1,49 @@
+/*Напишите программу, находящую все простые числа Ченя не превосходящие n.*/
+
+#include <iostream>
+#include <cmath>
+
+long* chen(long n) {
+	long i = 0;
+	long* arr = new long[n];
+	while (i < n) {
+		arr[i] = 0;
+		i++;
+	}
+	i = 1;
+	while (i <= long(sqrt(n)) + 1) {
+		long j = i + i + 2;
+		while (j < n + 2) {
+			if (arr[j - 1] == 0) arr[j - 1] = i + 1;
+			j += i + 1;
+		}
+		i += 1;
+	}
+	i = 1;
+	long j = 1;
+	long* ans = new long[n];
+	while (i < n - 2) {
+		if ((arr[i + 2] == 0 || (arr[arr[i + 2] - 1] == 0 && arr[i / (arr[i + 2] - 1)] == 0)) && arr[i] == 0) {
+			ans[j] = i + 1;
+			j += 1;
+		}
+		i++;
+	}
+	ans[0] = j;
+	return ans;
+}
+
+
+int main() {
+	long n;
+	std::cin >> n;
+	n += 2;
+	long* arr = new long[n];
+	arr = chen(n);
+	long i = 1;
+	while (i < arr[0]) {
+		std::cout << arr[i] << " ";
+		i++;
+	}
+	return 0;
+}
--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,47 +1,48 @@
-﻿#include <iostream>
+﻿/*Напишите программу, находящую все простые числа Ченя не превосходящие n.*/
+
+#include <iostream>
 #include <cmath>
 
-
-long reverse(long n, long len) {
-	long lim = pow(10, len - 1);
+long* chen(long n) {
 	long i = 0;
-	long res = 0;
-	while (i < len) {
-		res += n % 10 * lim;
-		n /= 10;
-		lim /= 10;
-		i += 1;
+	long* arr = new long[n];
+	while (i < n) {
+		arr[i] = 0;
+		i++;
 	}
-	return res;
+	i = 1;
+	while (i <= long(sqrt(n)) + 1) {
+		long j = i + i + 2;
+		while (j < n + 2) {
+			if (arr[j-1] == 0) arr[j - 1] = i + 1;
+			j += i + 1;
+		}
+		i++;
+	}
+	i = 1;
+	long j = 1;
+	long* ans = new long[n];
+	while (i < n - 2) {
+		if ((arr[i + 2] == 0 || (arr[arr[i + 2] - 1] == 0 && arr[i/(arr[i + 2] - 1)] == 0)) && arr[i] == 0) {
+			ans[j] = i + 1;
+			j++;
+		}
+		i++;
+	}
+	ans[0] = j;
+	return ans;
 }
 
 
 int main() {
 	long n;
 	std::cin >> n;
-	long i = pow(10, n - n / 2 - 1);
-	long len = n - n / 2;
-	long j;
-	int check;
-	while (i < pow(10, n - n / 2)) {
-		j = 0;
-		while (j < n - n / 2 - len) {
-			std::cout << 0;
-			j++;
-		}
-		if (i != 0 and n != 1) {
-			if (n % 2 == 1) std::cout << i / 10;
-			else std::cout << i;
-		}
-		if (i % 10 == 0) std::cout << 0;
-		if (i % 100 == 0) std::cout << 0;
-		std::cout << reverse(i, len);
-		j = 0;
-		while (j < n - n / 2 - len) {
-			std::cout << 0;
-			j++;
-		}
-		std::cout << "\n";
+	n += 2;
+	long* arr = new long[n];
+	arr = chen(n);
+	long i = 1;
+	while (i < arr[0]) {
+		std::cout << arr[i] << " ";
 		i++;
 	}
 	return 0;
