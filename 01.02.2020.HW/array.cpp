cacdbe1a804fe36eb86eb7087924d428cf5d2814
ReOT20 <ReOT20@users.noreply.github.com>
2020-01-31 23:20:21 +0300

    Vector struct
    Added vector struct to array.cpp

--- 01.02.2020.HW/array.cpp
+++ 01.02.2020.HW/array.cpp
@@ -39,7 +39,7 @@ struct Array {
 		return size;
 	}
 
-	Array& operator= (Array& b) {
+	Array& operator= (const Array& b) {
 		if (size != b.size) {
 			if (size != 0) {
 				delete[] data;
@@ -56,11 +56,84 @@ struct Array {
 };
 
 
+struct Vector {
+	int size = 0;
+	int* data;
+	int capacity = 0;
+
+	Vector() {}
+
+	Vector(int x) {
+		size = x;
+		capacity = size * 2;
+		data = new int[capacity];
+	}
+
+	Vector(const Vector& A) {
+		size = A.size;
+		capacity = A.capacity;
+		data = new int[capacity];
+		for (int i = 0; i < size; i++) {
+			data[i] = A.data[i];
+		}
+	}
+
+	~Vector() {
+		delete[] data;
+	}
+
+	int& operator[] (int i) {
+		if (i < 0 || i >= size) {
+			std::cout << "Err";
+			return data[0];
+		}
+		return data[i];
+	}
+
+	int& Size() {
+		return size;
+	}
+
+	int& Capacity() {
+		return capacity;
+	}
+
+	Vector& operator= (const Vector& b) {
+		if (capacity != b.capacity) {
+			if (capacity != 0) {
+				delete[] data;
+				int* data;
+			}
+			size = b.size;
+			capacity = b.capacity;
+			data = new int[capacity];
+		}
+		for (int i = 0; i < size; i++) {
+			data[i] = b.data[i];
+		}
+		return *this;
+	}
+
+	void push_back(int x) {
+		if (size == capacity) {
+			if (capacity == 0) {
+				capacity = 1;
+			}
+			capacity *= 2;
+			int* temp = new int[capacity];
+			for (int i = 0; i < size; i++) {
+				temp[i] = data[i];
+			}
+			if (size != 0)
+				delete[] data;
+			data = temp;
+		}
+		data[size] = x;
+		size++;
+	}
+};
+
+
 int main() {
-	Array a(1);
-	a[0] = 2;
-	Array b(4);
-	b = a;
-	cout << (a[0] == b[0] && a.Size() == b.Size());
 	return 0;
 }
