5a5f985b78f3bbbf517a134cbaf535025c6f8c1d
ReOT20 <57290617+ReOT20@users.noreply.github.com>
2020-04-18 23:02:34 +0300

    Raid5 example
    Example of use of raid5 technology

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,43 +3,39 @@
 using namespace std;
 
 
-int min(int a, int b, int c) {
-	if (a < b and a < c) return a;
-	if (b < c) return b;
-	return c;
-}
-
-
 int main() {
-	cout << "Enter 2 words\n";
-	string a, b;
-	a = "Eh bien, mon prince. Gênes et Lucques ne sont plus que des apanages, des поместья, de la famille Buonaparte";
-	b = "Non, je vous préviens que si vous ne me dites pas que nous avons la guerre, si vous vous permettez encore de pallier toutes les infamies, toutes les atrocités de cet Antichrist (ma parole, j'y crois) — je ne vous connais plus, vous n'êtes plus mon ami, vous n'êtes plus мой верный раб, comme vous dites";
-	int dc, inc, rc;
-	cout << "Enter delete cost\n";
-	cin >> dc;
-	cout << "Enter insert cost\n";
-	cin >> inc;
-	cout << "Enter replace cost\n";
-	cin >> rc;
-	int** arr = new int* [a.length()];
-	for (int i = 0; i < a.length(); i++) {
-		arr[i] = new int[b.length()];
+	bool* disk_1 = new bool[8];
+	bool* disk_2 = new bool[8];
+	bool* disk_3 = new bool[8];
+	bool* disk_4 = new bool[8];
+	cout << "Input disk 1 data:\n";
+	for (int i = 0; i < 8; i++) {
+		cin >> disk_1[i];
 	}
-	arr[0][0] = 0;
-	for (int j = 1; j < b.length(); j++) {
-		arr[0][j] = arr[0][j - 1] + inc;
+	cout << "Input disk 2 data:\n";
+	for (int i = 0; i < 8; i++) {
+		cin >> disk_2[i];
 	}
-	for (int i = 1; i < a.length(); i++) {
-		arr[i][0] = arr[i - 1][0] + dc;
-		for (int j = 1; j < b.length(); j++) {
-			if (a[i] != b[j]) {
-				arr[i][j] = min(arr[i - 1][j] + dc, arr[i][j - 1] + inc, arr[i - 1][j - 1] + rc);
-			}
-			else {
-				arr[i][j] = arr[i - 1][j - 1];
-			}
-		}
+	cout << "Input disk 3 data:\n";
+	for (int i = 0; i < 8; i++) {
+		cin >> disk_3[i];
 	}
-	cout << arr[a.length() - 1][b.length() - 1];
+	cout << "Generating controll sum...\n";
+	for (int i = 0; i < 8; i++) {
+		disk_4[i] = disk_1[i] ^ disk_2[i] ^ disk_3[i];
+	}
+	cout << "Loosing disk 2 data...\n";
+	for (int i = 0; i < 8; i++) {
+		disk_2[i] = 0;
+	}
+	cout << "Restoring disk 2 data...\n";
+	for (int i = 0; i < 8; i++) {
+		disk_2[i] = disk_1[i] ^ disk_3[i] ^ disk_4[i];
+	}
+	cout << "Completed!\n";
+	cout << "Reading disk 2 data:\n";
+	for (int i = 0; i < 8; i++) {
+		cout << disk_2[i] << " ";
+	}
+	return 0;
 }
